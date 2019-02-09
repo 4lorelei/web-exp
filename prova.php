8edee45c4e3a2970b4018a63abae5cf42ffe9354
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-02-09 18:00:50 +0100

    Add files via upload

--- prova.php
+++ prova.php
@@ -122,6 +122,15 @@
 			require_once "data.php"; 
 			echo "</hr>"; 
 		?> 
+		
+		<article>
+			<header>
+				<h1>Most important heading here</h1>
+				<h3>Less important heading here</h3>
+				<p>Some additional information here</p>
+			</header>
+			<p>Lorem Ipsum dolor set amet....</p>
+		</article>
 
 	</body> 
 </html> 
