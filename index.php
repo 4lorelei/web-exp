4ff16c3461c4795c6dc726c1d0f550b0ae3dcf31
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-02-09 17:11:21 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 include('login.php'); // Includes Login Script
-	if(isset($_SESSION['login_user'])){
-		header("location: profile.php");
-	}
+if(isset($_SESSION['login_user'])){
+	header("location: profile.php");
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -19,9 +19,11 @@ include('login.php'); // Includes Login Script
 				<form action="" method="post">
 					<label>UserName :</label>
 					<input id="name" name="username" placeholder="username" type="text">
+					
 					<label>Password :</label>
 					<input id="password" name="password" placeholder="**********" type="password">
-					<input name="submit" type="submit" value=" Login ">
+					
+					<input name="submit" type="submittt" value=" Login ">
 					<span><?php echo $error; ?></span>
 				</form>
 			</div>
