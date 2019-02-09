9c3bb999e450d7bfaa4b52a7a42ea86e8eb95818
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-02-09 18:16:21 +0100

    Add files via upload

---
+++ prova2.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html> 
+
+<html> 
+    <head> 
+        <meta charset="UTF-8"> 
+        <title></title> 
+        <link rel="stylesheet" href="aggiungi.css"> 
+		<style type="text/css"> 
+			#intestazione
+			{ 
+				 width:100%; 
+				 height:100px; 
+				 position:absolute; 
+				 font-family: Comic Sans MS; 
+				 top:0px; 
+				 left:0px; 
+				 text-align: center; 
+			} 
+			#divhr 
+			{ 
+				position:absolute; 
+				top: 105px;     
+				font-family:Comic Sans MS; 
+			} 
+			#footer 
+			{
+				position: fixed; 
+				bottom: 0; 
+				left: 0; 
+				text-align: center; 
+				background: white; 
+				color: black; 
+				font-family: Comic Sans MS; 
+				font-size: 15px; 
+				width: 100%; 
+			   /* padding: 5px 0;*/ 
+				opacity: 0.9; 
+				-moz-opacity: 0.9; 
+				filter: alpha(opacity=90); 
+			} 
+
+			/* Aggiungi*/ 
+
+			.bottone 
+			{ 
+				-moz-border-radius: 8px; 
+				-webkit-border-radius: 8px; 
+				border-radius: 8px; 
+				 
+				-moz-box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 2px 2px rgba(0,0,0,0.2); 
+				-webkit-box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 2px 2px rgba(0,0,0,0.2); 
+				box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 2px 2px rgba(0,0,0,0.2); 
+				 
+				border: 1px solid; 
+				padding: 8px 12px; 
+				 
+				color: #fff; 
+				font-family: "Myriad Pro", Arial, Tahoma, sans-serif; 
+				font-size: 1.4em; 
+				font-weight: bold; 
+				text-shadow: -1px -1px rgba(0,0,0,0.1), 0 0 15px rgba(255,255,255,0.75); 
+				white-space: nowrap; 
+				 
+				background: #777; 
+				border-color: #777; 
+				 
+				text-decoration: none; 
+				font-style: normal; 
+				text-transform: none; 	 
+			} 
+			.bottone:hover, 
+			.bottone:focus 
+			{ 
+				position: relative; 
+				bottom: 1px; 
+
+				-moz-box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 3px 2px rgba(0,0,0,0.2); 
+				-webkit-box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 3px 2px rgba(0,0,0,0.2); 
+				box-shadow: inset 0 -1px 3px rgba(255,255,255,0.5), 0 3px 2px rgba(0,0,0,0.2); 
+			}     
+
+			.ClasseDelDiv
+			{ 
+				display:inline; 
+				margin : 0.5%; 
+			} 
+			
+			
+			
+			div {
+  border: 1px solid blue;
+  text-align: center;
+}
+
+#matrix {
+  border: 1px solid red;
+  margin: 0 auto;
+  padding: 5px;
+}
+
+#matrix tr, td {
+  padding: 5px;
+}
+
+#matrix button {
+  height: 80px;
+  width: 150px;
+  border: none;
+  font-family: helvetica;
+  font-weight: bold;
+}
+
+#b1, #b3 {
+  background-color: rgba(255, 153, 51, 0.5);
+}
+
+#b1:hover, #b3:hover {
+  background-color: rgb(255, 153, 51);
+}
+
+#b2 {
+  background-color: rgba(15, 138, 15, 0.5);
+}
+
+#b2:hover {
+  background-color: rgb(15, 138, 15);
+}
+
+
+#b4 {
+  background-color: rgba(189, 15, 15, 0.6);
+}
+
+#b4:hover {
+  background-color: rgb(189, 15, 15);
+}
+
+		</style> 
+    </head> 
+    <header>         
+		<div id = "intestazione"> 
+            <h1>Gestione iscrizione prova</h1> 
+            <hr> 
+        </div>   
+    </header>  
+	
+	<footer> 
+		<div id ="footer"> 
+			<hr> 
+				<h1>Questa è una prova del footer fondo pagina</h1> 
+			</hr> 
+		</div> 
+	</footer> 
+     
+    <body>  
+    
+		
+		
+		<article>
+			<h1>Most important heading here</h1>
+			<h3>Less important heading here</h3>
+			<p>Some additional information here</p>
+			<p>Lorem Ipsum dolor set amet....</p>
+		</article>
+		
+		<div>
+		  <table id="matrix">
+			<tr>
+			  <td>
+				<button id="b1">
+				  <span>1</span>
+				</button></td>
+			  <td><button id="b2">2</button></td>
+			</tr>
+		  
+			<tr>
+			  <td><button id="b3">3</button></td>
+			  <td><button id="b4">4</button></td>
+			</tr>
+		  </table>
+		</div>
+
+	</body> 
+</html> 
+
+
