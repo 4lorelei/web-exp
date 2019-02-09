1d43ded873fea3f0d6981f26a2bf44cbc9f2532a
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-02-09 18:05:40 +0100

    Add files via upload

--- prova.php
+++ prova.php
@@ -124,11 +124,9 @@
 		?> 
 		
 		<article>
-			<header>
-				<h1>Most important heading here</h1>
-				<h3>Less important heading here</h3>
-				<p>Some additional information here</p>
-			</header>
+			<h1>Most important heading here</h1>
+			<h3>Less important heading here</h3>
+			<p>Some additional information here</p>
 			<p>Lorem Ipsum dolor set amet....</p>
 		</article>
 
